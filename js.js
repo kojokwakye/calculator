a3d032abd28bc720b74c3d6909c96418387dfa0d
kojokwakye <kojokwakye808@gmail.com>
2023-09-09 03:06:49 +0000

    add  operate()

--- js.js
+++ js.js
@@ -1,26 +1,27 @@
-let operators = {
-  "+": function (a, b) {
-    return a + b;
-  },
-  "-": function (a, b) {
-    return a - b;
-  },
-  "*": function (a, b) {
-    return a * b;
-  },
-  "/": function (a, b) {
-    return a / b;
-  },
-};
+function operate() {
+  let a = Number(window.prompt("Type a number", ""));
+  let operator = window.prompt("Type an operator (+, -, *, /)", "");
+  let b = Number(window.prompt("Type a number", ""));
+  console.log(a, operator, b);
 
-let addition = "+";
-console.log(operators[addition](3, 5));
-
-let subtraction = "-";
-console.log(operators[subtraction](3, 5));
-
-let multiply = "*";
-console.log(operators[multiply](3, 5));
-
-let divide = "/";
-console.log(operators[divide](3, 5));
+  let operators = {
+    "+": function (a, b) {
+      return a + b;
+    },
+    "-": function (a, b) {
+      return a - b;
+    },
+    "*": function (a, b) {
+      return a * b;
+    },
+    "/": function (a, b) {
+      return a / b;
+    },
+  };
+  if (operators.hasOwnProperty(operator)) {
+    return operators[operator](a, b);
+  } else {
+    return "invalid operator";
+  }
+}
+console.log(operate());
