7c867d4159d42faba6a948b99dca6abbab4d31ea
kojokwakye <kojokwakye808@gmail.com>
2023-09-09 03:23:43 +0000

    ??

--- js.js
+++ js.js
@@ -1,27 +1,42 @@
+// functions for each operation
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
 function operate() {
   let a = Number(window.prompt("Type a number", ""));
   let operator = window.prompt("Type an operator (+, -, *, /)", "");
   let b = Number(window.prompt("Type a number", ""));
   console.log(a, operator, b);
 
-  let operators = {
-    "+": function (a, b) {
-      return a + b;
-    },
-    "-": function (a, b) {
-      return a - b;
-    },
-    "*": function (a, b) {
-      return a * b;
-    },
-    "/": function (a, b) {
-      return a / b;
-    },
-  };
-  if (operators.hasOwnProperty(operator)) {
-    return operators[operator](a, b);
+  let result;
+
+  // call the appropriate function based on the operator
+  if (operator === "+") {
+    result = add(a, b);
+  } else if (operator === "-") {
+    result = subtract(a, b);
+  } else if (operator === "*") {
+    result = multiply(a, b);
+  } else if (operator === "/") {
+    result = divide(a, b);
   } else {
-    return "invalid operator";
+    result = "Invalid operator";
   }
+
+  return result;
 }
+
 console.log(operate());
