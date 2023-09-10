f366434f7bf186bb5b58a11ff37bca451785c514
kojokwakye <kojokwakye808@gmail.com>
2023-09-10 01:14:50 +0000

    add ac fxn

--- js.js
+++ js.js
@@ -1,3 +1,19 @@
+const screen = document.getElementById("screen");
+const btns = document.querySelectorAll(".digits, .operator-btn");
+
+for (const btn of btns) {
+  btn.addEventListener("click", () => {
+    screen.textContent = btn.textContent;
+  });
+}
+
+// eraser btn
+const ac = document.getElementById("clear-screen");
+ac.addEventListener("click", clearDisplay);
+function clearDisplay() {
+  screen.textContent = "";
+}
+
 // functions for each operation
 function add(a, b) {
   return a + b;
@@ -15,28 +31,28 @@ function divide(a, b) {
   return a / b;
 }
 
-function operate() {
-  let a = Number(window.prompt("Type a number", ""));
-  let operator = window.prompt("Type an operator (+, -, *, /)", "");
-  let b = Number(window.prompt("Type a number", ""));
-  console.log(a, operator, b);
+// function operate() {
+//   // let a = Number(window.prompt("Type a number", ""));
+//   // let operator = window.prompt("Type an operator (+, -, *, /)", "");
+//   // let b = Number(window.prompt("Type a number", ""));
+//   // console.log(a, operator, b);
 
-  let result;
+//   let result;
 
-  // call the appropriate function based on the operator
-  if (operator === "+") {
-    result = add(a, b);
-  } else if (operator === "-") {
-    result = subtract(a, b);
-  } else if (operator === "*") {
-    result = multiply(a, b);
-  } else if (operator === "/") {
-    result = divide(a, b);
-  } else {
-    result = "Invalid operator";
-  }
+//   // call the appropriate function based on the operator
+//   if (operator === "+") {
+//     result = add(a, b);
+//   } else if (operator === "-") {
+//     result = subtract(a, b);
+//   } else if (operator === "*") {
+//     result = multiply(a, b);
+//   } else if (operator === "/") {
+//     result = divide(a, b);
+//   } else {
+//     result = "Invalid operator";
+//   }
 
-  return result;
-}
+//   return result;
+// }
 
-console.log(operate());
+// console.log(operate());
