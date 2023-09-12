83f53118f40095be65d8ff7d6c0a81ca88b99619
kojokwakye <kojokwakye808@gmail.com>
2023-09-12 02:12:04 +0000

    prototype

--- js.js
+++ js.js
@@ -1,60 +1,104 @@
-const input = document.getElementById("screen");
+const input = document.getElementById("screen"); // main screen
 const numberBtns = document.querySelectorAll(".digits");
 const operatorBtns = document.querySelectorAll(".operator-btn");
-const dotBtn = document.querySelector(".dot");
+const dotBtn = document.querySelector(".dot"); // point
+const solve = document.getElementById("equal"); // equal to
 const ac = document.getElementById("clear-screen"); // clear btn
 
 // display operation
-let displayCurrent = document.querySelector("#current");
-let displayPrevious = document.querySelector("#previous");
+let displayCurrent = document.querySelector("#current"); // green screen
+let displayPrevious = document.querySelector("#previous"); // blue screen
+
+// store operands
+let a = ""; // store the first operand
+let operator = ""; // store the operator
+let waitForSecondOperand = false; //  wait for the second operand
 
 numberBtns.forEach((btn) => {
   btn.addEventListener("click", (event) => {
-    displayCurrent.textContent += event.target.textContent;
+    if (waitForSecondOperand) {
+      displayCurrent.textContent = event.target.textContent;
+      waitForSecondOperand = false;
+    } else {
+      displayCurrent.textContent += event.target.textContent;
+    }
   });
 });
 
 operatorBtns.forEach((btn) => {
   btn.addEventListener("click", (event) => {
-    displayCurrent.textContent += event.target.textContent;
+    if (a && operator && !waitForSecondOperand) {
+      b = displayCurrent.textContent;
+      operate();
+      a = displayCurrent.textContent;
+    } else {
+      a = displayCurrent.textContent;
+    }
+
+    operator = event.target.textContent;
+    waitForSecondOperand = true; // clear the current input and start a new input for the second operand.
   });
 });
+
+// clear both screens
+ac.addEventListener("click", () => {
+  a = "";
+  operator = "";
+  displayCurrent.textContent = "";
+  displayPrevious.textContent = "";
+});
+
+// equal to sign
+solve.addEventListener("click", () => {
+  if (a && operator && !waitForSecondOperand) {
+    b = displayCurrent.textContent;
+    operate();
+    a = "";
+    operator = "";
+    waitForSecondOperand = true; // clear the current input and start a new input for the second operand.
+  }
+});
+
 // functions for each operation
 function add(a, b) {
-  return a + b;
+  return parseFloat(a) + parseFloat(b);
 }
 
 function subtract(a, b) {
-  return a - b;
+  return parseFloat(a) - parseFloat(b);
 }
 
 function multiply(a, b) {
-  return a * b;
+  return parseFloat(a) * parseFloat(b);
 }
 
 function divide(a, b) {
-  return a / b;
+  return parseFloat(a) / parseFloat(b);
 }
 
-// three variables for each of the parts of a calculator operation.
-let a = "";
-let b = "";
-let operator = "";
-
-function operate(a, operator, b) {
+function operate() {
   let result;
-  // call the appropriate function based on the operator
-  if (operator === "+") {
-    result = add(a, b);
-  } else if (operator === "-") {
-    result = subtract(a, b);
-  } else if (operator === "*") {
-    result = multiply(a, b);
-  } else if (operator === "/") {
-    result = divide(a, b);
-  } else {
-    result = "Invalid operator";
+  switch (operator) {
+    case "+":
+      result = add(a, b);
+      break;
+    case "-":
+      result = subtract(a, b);
+      break;
+    case "*":
+      result = multiply(a, b);
+      break;
+    case "/":
+      if (b === "0") {
+        result = "Error: Division by zero";
+      } else {
+        result = divide(a, b);
+      }
+      break;
+    default:
+      result = "Invalid operator";
   }
 
-  return result;
+  displayPrevious.textContent = `${a} ${operator} ${b} =`;
+  displayCurrent.textContent = result;
 }
